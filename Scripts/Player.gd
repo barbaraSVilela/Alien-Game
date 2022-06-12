e72d8a9fe5b5fcb1efb040daf6782da5852a386c
Barbara Vilela <barbara.vilela@edu.pucrs.br>
2022-06-12 18:30:09 -0300

    main charcter moving and jumping and background moving

---
+++ Scripts/Player.gd
@@ -0,0 +1,40 @@
+extends KinematicBody2D
+
+var velocity = Vector2.ZERO
+export var jump_velocity = 500.0
+export var gravity_scale = 20.0
+var canJump = true
+var doubleJump = 2
+onready var animation = $AnimatedSprite
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	animation.play("run")
+
+func _physics_process(delta):
+	velocity.y += gravity_scale
+	move_and_collide(velocity*delta)
+	
+func _input(event):
+	if event.is_action_pressed("jump"):
+		if doubleJump > 0:
+			velocity = Vector2.ZERO
+			velocity.y -= jump_velocity
+			animation.play("jump")
+			doubleJump = doubleJump - 1
+
+
+
+
+func _on_Area2D_body_entered(body):
+	if body is StaticBody2D:
+		canJump = true
+		doubleJump=2
+		animation.play("run")
+	
+
+
+func _on_Area2D_body_exited(body):
+	if body is StaticBody2D:
+		canJump= false
---
+++ Scripts/background.gd
@@ -0,0 +1,17 @@
+extends Sprite
+
+const velocity: float = -2.0
+var g_texture_width: float =0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	g_texture_width = texture.get_size().x * scale.x
+
+
+func _process(delta):
+		position.x += velocity
+		_attemptReposition()
+	
+func _attemptReposition():
+	if position.x< -g_texture_width:
+		position.x += 2*g_texture_width
